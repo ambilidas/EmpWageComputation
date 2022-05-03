d56be57f13469ef04e0e7dd8d8913952c092bec9
Ambili das v s <ambili.kan@gmail.com>
2022-05-03 11:09:16 +0530

    check employee is present or not

---
+++ EmpWageComputation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class EmpWageComputation
+    {
+        Random generateNum = new Random();
+
+        public void CheckEmpIsPresentOrNot()
+        {
+            int num = generateNum.Next(0,2);
+
+            if(num == 1)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
+            EmpWageComputation emp = new EmpWageComputation();
+            emp.CheckEmpIsPresentOrNot();
         }
     }
 }
